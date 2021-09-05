4ea7a3c2fe5eb4d4069dc4be0f39149f7573be3d
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-09-05 18:43:24 +0300

    setup interface

---
+++ ColorizerApp/SettingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingViewController.swift
+//  ColorizerApp
+//
+//  Created by Илья Нечаев on 05.09.2021.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func doneButtonPressed() {
+    }
+}
